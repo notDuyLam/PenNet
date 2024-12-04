16e8b8371f30e8e894724cbdfbba124bf91187f7
NgocDang <ngocdang240604@gmail.com>
2024-12-04 22:02:51 +0700

    create hbs and complete home page

--- app.js
+++ app.js
@@ -13,19 +13,8 @@ app.set('view engine', 'hbs');
 app.set('views', './views');
 
 
-const user = {
-    id: 1,
-    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmjxkzji2G1q-FKeA842yBCrz5xOW_HpfdOQ&s",
-    cover_photo: "https://event.danang.vn/wp-content/uploads/2023/06/background-gala-dinner-dep-19-800x377.jpg",
-    name: "Nguyễn Văn A",
-    followers: [32,47],
-    following: [12,13],
-    posts: 4
-}
-
 app.get('/', (req, res) => {
-    // res.send('Hello World!');
-    res.render('home', {user: user});
+    res.send('Hello World!');
 });
 
 app.listen(port, () => {
