f94d77ca7f118d9547b867ef4eef55912ad9c716
notDuyLam <22120181@student.hcmus.edu.vn>
2024-12-06 20:24:13 +0700

    edit some elements

--- routes.js
+++ routes.js
@@ -5,26 +5,17 @@ const router = express.Router();
 
 // Home route
 router.get('/', (req, res) => {
-    res.render('home', {
-        // title: 'Home Page',
-        // name: 'John Doe'
-    });
+    res.render('home', {});
 });
 
 // Login route
 router.get('/login', (req, res) => {
-    res.render('login', {
-        // title: 'Home Page',
-        // name: 'John Doe'
-    });
+    res.render('login', {});
 });
 
 // Signup route
 router.get('/signup', (req, res) => {
-    res.render('signup', {
-        // title: 'Home Page',
-        // name: 'John Doe'
-    });
+    res.render('signup', {});
 });
 
 module.exports = router;
