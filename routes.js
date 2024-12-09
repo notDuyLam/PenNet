0f49ae8c018a3073fed7ee718b822dfcc30b64cc
NgocDang <ngocdang240604@gmail.com>
2024-12-09 22:15:43 +0700

    personProfile, chua co reviews, setting

--- routes.js
+++ routes.js
@@ -8,6 +8,10 @@ router.get('/', (req, res) => {
     res.render('home', {});
 });
 
+router.get('/user/id=:id', (req, res) => {
+    res.render('personProfile', {});
+});
+
 // Login route
 router.get('/login', (req, res) => {
     res.render('login', {});
