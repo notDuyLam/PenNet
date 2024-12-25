9415f33b2283a6657335d9eaf0ab1621c03c0eba
Huynh Tan Loc <12a1huynhtanloc@gmail.com>
2024-12-26 00:03:58 +0700

    Temp view for post

--- public/js/home.js
+++ public/js/home.js
@@ -1,4 +1,43 @@
 document
+  .getElementById("image-upload")
+  .addEventListener("change", function (event) {
+    const fileInput = event.target;
+    const files = fileInput.files;
+    const previewContainer = document.createElement("div");
+    previewContainer.id = "image-preview-container";
+    previewContainer.classList.add("flex", "flex-wrap", "mt-4");
+
+    // Clear previous previews
+    const existingPreview = document.getElementById("image-preview-container");
+    if (existingPreview) {
+      existingPreview.remove();
+    }
+
+    const wrapperDiv = document.createElement("div");
+    wrapperDiv.classList.add("flex", "justify-around");
+
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const reader = new FileReader();
+
+      reader.onload = function (e) {
+        const img = document.createElement("img");
+        img.src = e.target.result;
+        img.classList.add("h-20", "w-20", "object-cover", "mr-2", "mb-2");
+        previewContainer.appendChild(img);
+      };
+
+      reader.readAsDataURL(file);
+    }
+
+    wrapperDiv.appendChild(previewContainer);
+    const targetDiv = document.querySelector(
+      ".flex.items-center.mb-4.data-send"
+    );
+    targetDiv.parentNode.insertBefore(wrapperDiv, targetDiv.nextSibling);
+  });
+
+document
   .querySelector("#post-form")
   .addEventListener("submit", async function (event) {
     event.preventDefault();
