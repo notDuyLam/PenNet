09edf56f8f83859090027baf45ab541ff1e49962
notDuyLam <22120181@student.hcmus.edu.vn>
2024-12-27 10:57:06 +0700

    Add isBanned field to User model and create ban view

--- apps/user/model.js
+++ apps/user/model.js
@@ -56,6 +56,10 @@ User.init(
       type: DataTypes.BOOLEAN,
       defaultValue: false,
     },
+    isBanned: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
   },
   {
     sequelize,
