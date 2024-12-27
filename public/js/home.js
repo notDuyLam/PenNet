3cbd3f3688810cd00d1238add4e5ed207e3a829c
NgocDang <ngocdang240604@gmail.com>
2024-12-28 00:04:37 +0700

    ui chinh sua post, them xoa anh

--- public/js/home.js
+++ public/js/home.js
@@ -37,16 +37,35 @@ document
     targetDiv.parentNode.insertBefore(wrapperDiv, targetDiv.nextSibling);
   });
 
+let isUpPost = false;
 document
   .querySelector("#post-form")
   .addEventListener("submit", async function (event) {
     event.preventDefault();
-    alert("Please wait while your post is being submitted...");
+    //alert("Please wait while your post is being submitted...");
+    if(isUpPost){
+        return;
+    }
+    isUpPost = true;
+    const spinner = $(`
+        <div class="w-full h-full fixed top-0 left-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <div class="spinner"> </div> 
+        </div>`);
+    $('body').append(spinner).addClass('no-scroll');
+
     const form = event.target;
     const formData = new FormData();
     const fileInput = document.getElementById("image-upload");
     const files = fileInput.files;
     const content = document.querySelector("textarea[name='content']").value;
+    // Kiểm tra dữ liệu trước khi gửi về server
+    if(!content && files.length === 0) {
+      alert("Please enter content or upload images");
+      $('body').removeClass('no-scroll');
+      spinner.remove();
+      isUpPost = false;
+      return;
+    }
     formData.append("content", content);
     if (files.length > 0) {
       for (let i = 0; i < files.length; i++) {
@@ -61,12 +80,19 @@ document
 
       if (response.ok) {
         // Handle successful response
-        alert("Post submitted successfully");
+        //alert("Post submitted successfully");
+        location.reload();
       } else {
         // Handle error response
         alert("Error submitting post");
       }
+      $('body').removeClass('no-scroll');
+      spinner.remove();
+      isUpPost = false;
     } catch (error) {
       alert("Error submitting post", error);
+      $('body').removeClass('no-scroll');
+      spinner.remove();
+      isUpPost = false;
     }
   });
